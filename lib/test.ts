97f2a02237fcc0fca1d0b0b012a00e69e8b72ce2
Luis Finke <luisfinke@gmail.com>
2025-05-24 13:11:58 -0400

    add getter for film info in tests

--- lib/test.ts
+++ lib/test.ts
@@ -2,6 +2,18 @@
 import * as letterboxd from './index';
 
 (async () => {
+	console.log("Testing getFilmInfo");
+	try {
+		const result = await letterboxd.getFilmInfo({tmdbId: "64122"});
+		console.log(JSON.stringify(result, null, '\t'));
+	} catch(error) {
+		console.error(error);
+	}
+	console.log();
+	console.log();
+	console.log();
+	return;
+
 	console.log("Testing getFilmHrefFromExternalID");
 	try {
 		const result = await letterboxd.getFilmHrefFromExternalID({tmdbId:'1226578'});
